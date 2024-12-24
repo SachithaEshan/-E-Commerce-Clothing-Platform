ce48ac2794846d29853940dfc2d3f1146a57ad47
Goigoda Siriwardhana <10898919@students.plymouth.ac.uk>
2024-12-24 18:57:28 +0530

    Add products

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,7 +1,21 @@
 import logo from "./wahal_d3c0pi6.png";
 import cart_icon from "./shopping-cart-line.png";
 import search_icon from "./search-line.png";
-import p_img1 from "./1568868097.jpeg";
+import p_img1 from "./p_img1.jpeg";
+import p_img2 from "./p_img2.jpeg";
+import p_img3 from "./p_img3.jpeg";
+import p_img4 from "./p_img4.jpeg";
+import p_img5 from "./p_img5.jpeg";
+import p_img6 from "./p_img6.jpeg";
+import p_img7_1 from "./p_img7_1.jpeg";
+import p_img7_2 from "./p_img7_2.jpeg";
+import p_img8_1 from "./p_img8_1.jpeg";
+import p_img8_2 from "./p_img8_2.jpeg";
+import p_img9_1 from "./p_img9_1.jpeg";
+import p_img9_2 from "./p_img9_2.jpeg";
+import p_img10_1 from "./p_img10_1.jpeg";
+import p_img10_2 from "./p_img10_2.jpeg";
+
 import profile_icon from "./account-circle-line.png";
 import menu_icon from "./menu-line.png";
 import dropdown_icon from "./arrow-down-line.png";
@@ -22,34 +36,15 @@ export const assets = {
   quality_icon,
   customer_icon,
 };
-// export const assets = {
-//   logo,
-//   hero_img,
-//   cart_icon,
-//   dropdown_icon,
-//   exchange_icon,
-//   profile_icon,
-//   quality_icon,
-//   search_icon,
-//   star_dull_icon,
-//   star_icon,
-//   bin_icon,
-//   support_img,
-//   menu_icon,
-//   about_img,
-//   contact_img,
-//   stripe_logo,
-//   cross_icon,
-// };
 
 export const products = [
   {
     _id: "aaaaa",
-    name: "woman name the wear",
-    description: "a explnantion of the dress",
-    price: 100,
+    name: "Adidas Mens Lifestyle SweatShirt",
+    description: "All sizes are based in UK sizes",
+    price: 83,
     image: [p_img1],
-    category: "women",
+    category: "men",
     subCategory: "Topwear",
     sizes: ["S", "M", "L"],
     date: 123456879465,
@@ -57,12 +52,109 @@ export const products = [
   },
   {
     _id: "aaaab",
-    name: "anothername",
-    description: "fe deexlin",
-    price: 130,
-    image: [p_img1],
-    category: "women",
+    name: "Joma Mens Basketball Reversible Kit",
+    description:
+      "V-neck basketball T-shirt characterized by its reversible design. Includes Interlock fabric and Micro-Mesh technology enhancing breathability.",
+    price: 44,
+    image: [p_img2],
+    category: "men",
+    subCategory: "wear",
+    sizes: ["S", "M", "L"],
+    date: 12343453533221,
+    bestseller: true,
+  },
+  {
+    _id: "aaaac",
+    name: "Joma Mens Football Ss-Tshirt",
+    description: "All sizes are based in UK sizes",
+    price: 31,
+    image: [p_img3],
+    category: "men",
     subCategory: "Topwear",
+    sizes: ["S", "M", "L"],
+    date: 12343453533221,
+    bestseller: true,
+  },
+  {
+    _id: "aaaad",
+    name: "Adidas Mens Training T Shirt",
+    description: "All sizes are based in UK sizes",
+    price: 57,
+    image: [p_img4],
+    category: "men",
+    subCategory: "Topwear",
+    sizes: ["S", "M", "L"],
+    date: 12343453533221,
+    bestseller: true,
+  },
+  {
+    _id: "aaaae",
+    name: "Adidas Mens Training Pant",
+    description: "All sizes are based in UK sizes",
+    price: 76,
+    image: [p_img5],
+    category: "men",
+    subCategory: "Bottomwear",
+    sizes: ["S", "M", "L"],
+    date: 12343453533221,
+    bestseller: true,
+  },
+  {
+    _id: "aaaaf",
+    name: "Adidas Mens Running Short",
+    description: "All sizes are based in UK sizes",
+    price: 77,
+    image: [p_img6],
+    category: "men",
+    subCategory: "Bottomwear",
+    sizes: ["S", "M", "L"],
+    date: 12343453533221,
+    bestseller: true,
+  },
+  {
+    _id: "aaaag",
+    name: "Adidas Mens Running Short",
+    description: "All sizes are based in UK sizes",
+    price: 65,
+    image: [p_img7_1, p_img7_2],
+    category: "men",
+    subCategory: "Bottomwear",
+    sizes: ["S", "M", "L"],
+    date: 12343453533221,
+    bestseller: true,
+  },
+  {
+    _id: "aaaah",
+    name: "Adidas Originals Mens Lifestyle Shoe",
+    description: "All sizes are based in UK sizes",
+    price: 125,
+    image: [p_img8_1, p_img8_2],
+    category: "men",
+    subCategory: "Footmwear",
+    sizes: ["S", "M", "L"],
+    date: 12343453533221,
+    bestseller: true,
+  },
+  {
+    _id: "aaaai",
+    name: "New Balance Printed Women's Running Sleeveless Top",
+    description: "All sizes are based in UK sizes",
+    price: 24,
+    image: [p_img9_1, p_img9_2],
+    category: "women",
+    subCategory: "Topmwear",
+    sizes: ["S", "M", "L"],
+    date: 12343453533221,
+    bestseller: true,
+  },
+  {
+    _id: "aaaaj",
+    name: "Adidas Originals Womens Lifestyle Top",
+    description: "All sizes are based in UK sizes",
+    price: 53,
+    image: [p_img10_1, p_img10_2],
+    category: "women",
+    subCategory: "Topmwear",
     sizes: ["S", "M", "L"],
     date: 12343453533221,
     bestseller: true,
