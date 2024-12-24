18517556997fc76ad9283fdc4018dca9c02545db
Goigoda Siriwardhana <10898919@students.plymouth.ac.uk>
2024-12-24 18:03:54 +0530

    React Project Setup & Create Pages and Routing & Create Home Page components

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,70 @@
+import logo from "./wahal_d3c0pi6.png";
+import cart_icon from "./shopping-cart-line.png";
+import search_icon from "./search-line.png";
+import p_img1 from "./1568868097.jpeg";
+import profile_icon from "./account-circle-line.png";
+import menu_icon from "./menu-line.png";
+import dropdown_icon from "./arrow-down-line.png";
+import hero_img from "./8278216_3852578.jpg";
+import exchange_icon from "./exchange-line.png";
+import quality_icon from "./verified-badge-line.png";
+import customer_icon from "./customer-service-line.png";
+
+export const assets = {
+  logo,
+  cart_icon,
+  profile_icon,
+  search_icon,
+  menu_icon,
+  dropdown_icon,
+  hero_img,
+  exchange_icon,
+  quality_icon,
+  customer_icon,
+};
+// export const assets = {
+//   logo,
+//   hero_img,
+//   cart_icon,
+//   dropdown_icon,
+//   exchange_icon,
+//   profile_icon,
+//   quality_icon,
+//   search_icon,
+//   star_dull_icon,
+//   star_icon,
+//   bin_icon,
+//   support_img,
+//   menu_icon,
+//   about_img,
+//   contact_img,
+//   stripe_logo,
+//   cross_icon,
+// };
+
+export const products = [
+  {
+    _id: "aaaaa",
+    name: "woman name the wear",
+    description: "a explnantion of the dress",
+    price: 100,
+    image: [p_img1],
+    category: "women",
+    subCategory: "Topwear",
+    sizes: ["S", "M", "L"],
+    date: 123456879465,
+    bestseller: true,
+  },
+  {
+    _id: "aaaab",
+    name: "anothername",
+    description: "fe deexlin",
+    price: 130,
+    image: [p_img1],
+    category: "women",
+    subCategory: "Topwear",
+    sizes: ["S", "M", "L"],
+    date: 12343453533221,
+    bestseller: true,
+  },
+];
