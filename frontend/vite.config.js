113b849a9d0040a96cfec8c2b505616fac4e14da
Goigoda Siriwardhana <10898919@students.plymouth.ac.uk>
2024-12-28 11:50:08 +0530

    test update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom", // Enables DOM testing
+    setupFiles: "./test/setup.js", // Optional: Path to setup file for test initialization
+  },
 });
