3ac50de69845f9f00fbed4cfe643c041357bcf97
Goigoda Siriwardhana <10898919@students.plymouth.ac.uk>
2024-12-28 22:10:50 +0530

    update backend connections

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-import connectDB from "./config/mongodb";
-import connectCloudinary from "./config/cloudinary";
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
 
 // config
 const app = express();
