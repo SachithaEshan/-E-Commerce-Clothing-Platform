c08907baf0527b4855c6662d4cc17504cb814d55
Goigoda Siriwardhana <10898919@students.plymouth.ac.uk>
2024-12-28 20:01:51 +0530

    test update

---
+++ backend/jest.config.js
@@ -0,0 +1,5 @@
+export const testMatch = [
+  "**/tests/**/*.test.js",
+  "**/?(*.)+(spec|test).[jt]s?(x)",
+];
+export const testPathIgnorePatterns = ["/node_modules/"];
---
+++ backend/tests/example.test.js
@@ -0,0 +1,4 @@
+// backend/tests/sample.test.js
+test("Sample test", () => {
+  expect(true).toBe(true);
+});
