2f1c0937b3f168796726a8d69c4286e57c78f792
Goigoda Siriwardhana <10898919@students.plymouth.ac.uk>
2024-12-30 02:57:49 +0530

    Update Jest

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,7 +1,4 @@
 export default {
   testMatch: ["**/tests/**/*.test.js", "**/?(*.)+(spec|test).[jt]s?(x)"],
   testPathIgnorePatterns: ["/node_modules/"],
-  transform: {
-    "^.+\\.js$": "babel-jest", // Use Babel to transform JavaScript files
-  },
 };
