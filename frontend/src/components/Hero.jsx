95895b62f51922471a479cd0cbc3adaf3b06f728
Handun-Alwis <10899150@students.plymouth.ac.uk>
2025-01-13 18:59:45 +0530

    Hero section updated

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -7,17 +7,11 @@ const Hero = () => {
   return (
     <div className="flex flex-col sm:flex-row border border-gray-400">
       <img className="w-full sm:w-full" src={assets.hero_img} alt="" />
-      <div className="absolute inset-0 flex items-center justify-center">
-        <p className="text-black text-sm sm:text-base  absolute -translate-x-[-75%] translate-y-[90px] max-w-lg ">
-          Gear up for success. Discover high-performance apparel designed to
-          move with you, whether you're training hard or pushing limits. Shop
-          the latest styles now and get ready to conquer every challenge.
-        </p>
-      </div>
+
       <div className="relative flex flex-col ">
         <button
           className="absolute right-1/2 transform -translate-x-[80%] translate-y-[-40px] bottom-40 bg-black text-white text-sm sm:text-base py-2 px-4 rounded hover:bg-gray-800 transition whitespace-nowrap w-64"
-          onClick={() => navigate("/collection")} // Navigates to /collection page
+          onClick={() => navigate("/collection")}
         >
           Shop Now
         </button>
